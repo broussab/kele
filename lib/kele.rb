0ef6f7055b7cbc1aec721e0927d8412447c53930
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-03-01 15:19:47 -0600

    Modified kele.rb

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,11 +2,17 @@ require 'httparty'
 
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1'
+  base_uri 'https://www.bloc.io/api/v1/'
 
   def initialize(email, pass)
-    response = self.class.post('/sessions', email, pass)
-    raise 'Invalid Credentials' unless response.code = 200
+    user_creds = {
+      body: {
+        email:  email,
+        password: pass
+      }
+    }
+    response = self.class.post('/sessions', user_creds)
+    raise 'Invalid Credentials' unless response.code == 200
     @auth_token = response['auth_token']
   end
 end
