3fd4473d596026c2ae1e08d1306571624ae0172e
Alyssa Broussard <alyssabroussard@legalshieldcorp.com>
2017-03-02 11:23:41 -0600

    User story 5 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -30,4 +30,28 @@ class Kele
     response = self.class.get(mentor_url, headers: { 'authorization' => @auth_token })
     parsed_response = JSON.parse(response.body)
   end
+
+  def get_messages(*page)
+    if page.empty?
+      response = self.class.get('/message_threads/', headers: { 'authorization' => @auth_token })
+    else
+      response = self.class.get('/message_threads/', values: { 'page' => page[0] }, headers: { 'authorization' => @auth_token })
+    end
+    parsed_response = JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, token, subject, text)
+    message_values = {
+      my_body: {
+        sender: sender.to_s,
+        recipient_id: recipient_id.to_i,
+        token: token.to_s,
+        subject: subject.to_s,
+        stripped_text: text.to_s
+      }
+    }
+    response = self.class.post('https://private-anon-b99b0892a6-blocapi.apiary-mock.com/api/v1/messages', values: message_values, headers: { 'authorization' => @auth_token })
+    raise 'Invalid Message' unless response.code == 200
+    puts 'Message successfully posted!' if response.code == 200
+  end
 end
